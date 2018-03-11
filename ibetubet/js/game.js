0a8e8728a79872d83f4a14e49b311f2abc1a35c8
andrewgwallace <andrew.g.wallace@gmail.com>
2018-03-11 18:09:19 -0400

    Add index.html form field event listeners

---
+++ ibetubet/js/game.js
@@ -0,0 +1,86 @@
+// Gameplay functions and variables
+
+let p1 = '';
+let p2 = '';
+let p1Bet;
+let p1Guess;
+let p2Bet;
+let p2Guess;
+let p1Diff = Math.abs(p1Guess - p2Bet);
+let p2Diff = Math.abs(p2Guess - p1Bet);
+let superTieCount = 0;
+
+let round = 1;
+let p1Points = 50;
+let p2Points = 50;
+let jackpot = 100;
+let p1Multiplier; // 1, 2, or 3
+let p2Multiplier; // 1, 2, or 3
+let p1RoundsWon = 0;
+let p2RoundsWon = 0;
+let gameWinner = '';
+
+function newGame() {
+
+}
+
+function playRound () {
+  getBetsandGuesses();
+  findRoundWinner();
+}
+
+function findRoundWinner() {
+  if (p1Diff < p2Diff) {
+    console.log(`${p1} wins with a difference of ${p1Diff}`);
+    p1RoundsWon++;
+    round++;
+    superTieCount = 0;
+    p1Points += (p2Bet * p2Multiplier) - (p1Multiplier * p1Diff);
+    p2Points -= p2Bet * p2Multiplier;
+    jackpot += p1Multiplier * p1Diff;
+    // isGameOver();
+  }
+  else if (p1Diff > p2Diff) {
+    console.log(`${p2} wins with a difference of ${p2Diff}`);
+    p2RoundsWon++;
+    rounds++;
+    superTieCount = 0;
+    p2Points += (p1Bet * p1Multipler) - (p2Multiplier * p2Diff);
+    p1Points -= p1bet * p1Multiplier;
+    jackpot += p2Multiplier * p2Diff;
+    // isGameOver();
+  } else if ( p1Diff === p2Diff && (p1Guess === p2Bet && p2Guess === p1Bet)) {
+    console.log("Super Tie!");
+    superTieCount++;
+    p1Points += (p1Bet * p1Multiplier) * superTieCount;
+    p2Points += (p2Bet * p2Multiplier) * superTieCount;
+    rounds++;
+  } else {
+    console.log("Tie round");
+    p1Points -= p1bet * p1Multiplier;
+    p2points -= p2bet * p2Multiplier;
+    jackpot += (p1bet * p1Multiplier) + (p2bet * p2Multiplier);
+  }
+  // isGameOver();
+}
+
+function isGameOver () {
+  findRoundWinner();
+  if (p1RoundsWon > 5 ) {
+    gameWinner = p1;
+    console.log(`${p1} Wins the Game!`);
+  } else if (p2RoundsWon > 5) {
+    console.log(`${p2} Wins the Game!`);
+  } else {
+    playRound();
+  }
+}
+
+function getBetsandGuesses () {
+  p1Bet = prompt("What is your bet, player 1?");
+  p1Guess = prompt("And what is your guess, P1?");
+  p2Bet = prompt("What is your bet, player 2?");
+  p2Guess = prompt("And what is your guess, P2?");
+  findRoundWinner();
+}
+// Local storage functionality to maintain game state
--- ibetubet/js/script.js
+++ ibetubet/js/script.js
@@ -1,90 +1,32 @@
-let p1 = "Andrew";
-let p2 = "Computer";
-let p1Bet = 4 ;
-let p1Guess = 7;
-let p2Bet = 5;
-let p2Guess = 9;
-let p1Diff = Math.abs(p1Guess - p2Bet);
-let p2Diff = Math.abs(p2Guess - p1Bet);
-let tie = false;
-let superTie = false
-let superTieCount = 0;
+// DOM Manipuation
+// Require game.js
 
-let round = 1;
-let p1Points = 50;
-let p2Points = 50;
-let jackpot = 100;
-let p1Multiplier = 3 // 1, 2, or 3
-let p2Multiplier = 2 // 1, 2, or 3
+document.addEventListener('DOMContentLoaded', function() {
 
+// Put index.html JS below this line.
+let onePlayer = document.querySelector('input[name="p1Name"]');
+let twoPlayer = document.querySelector('input[name="p2Name"]');
+let startButton = document.querySelector('#start');
+let isReady = false;
 
-let p1RoundsWon = 0;
-let p2RoundsWon = 0;
-let gameWinner = '';
+onePlayer.addEventListener('change', changeStartText);
+twoPlayer.addEventListener('change', changeStartText);
+startButton.addEventListener('click', checkReady);
 
+function checkReady() {
+
+}
+
+function changeStartText() {
+  if (onePlayer.value !== '' && twoPlayer.value !== '') {
+    startButton.innerHTML = "2 Player Start!";
+  } else if (onePlayer.value !== '' && twoPlayer.value === '') {
+    startButton.innerHTML = "Let's Play!";
+  } else {
+      startButton.innerHTML = "Enter player name(s)"
+  }
+}
+
+// Put game.html JS below this line.
 let scoreboard = document.querySelector('.scoreboard');
-
-function newGame() {
-
-}
-
-function playRound () {
-  getBetsandGuesses();
-  findRoundWinner();
-}
-
-function findRoundWinner() {
-  if (p1Diff < p2Diff) {
-    console.log(`${p1} wins with a difference of ${p1Diff}`);
-    p1RoundsWon++;
-    round++;
-    superTieCount = 0;
-    p1Points += (p2Bet * p2Multiplier) - (p1Multiplier * p1Diff);
-    p2Points -= p2Bet * p2Multiplier;
-    jackpot += p1Multiplier * p1Diff;
-    // isGameOver();
-  }
-  else if (p1Diff > p2Diff) {
-    console.log(`${p2} wins with a difference of ${p2Diff}`);
-    p2RoundsWon++;
-    rounds++;
-    superTieCount = 0;
-    p2Points += (p1Bet * p1Multipler) - (p2Multiplier * p2Diff);
-    p1Points -= p1bet * p1Multiplier;
-    jackpot += p2Multiplier * p2Diff;
-    // isGameOver();
-  } else if ( p1Diff === p2Diff && (p1Guess === p2Bet && p2Guess === p1Bet)) {
-    console.log("Super Tie!");
-    superTieCount++;
-    p1Points += (p1Bet * p1Multiplier) * superTieCount;
-    p2Points += (p2Bet * p2Multiplier) * superTieCount;
-    rounds++;
-  } else {
-    console.log("Tie round");
-    p1Points -= p1bet * p1Multiplier;
-    p2points -= p2bet * p2Multiplier;
-    jackpot += (p1bet * p1Multiplier) + (p2bet * p2Multiplier);
-  }
-  // isGameOver();
-}
-
-function isGameOver () {
-  if (p1RoundsWon > 5 ) {
-    console.log("Player 1 Wins the Game!");
-  } else if (p2RoundsWon > 5) {
-    console.log("Player 2 Wins the Game!");
-  } else {
-    findRoundWinner();
-    playRound();
-  }
-}
-
-function getBetsandGuesses () {
-  p1Bet = prompt("What is your bet, player 1?");
-  p1Guess = prompt("And what is your guess, P1?");
-  p2Bet = prompt("What is your bet, player 2?");
-  p2Guess = prompt("And what is your guess, P2?");
-  findRoundWinner();
-}
-
-findRoundWinner();
+});
