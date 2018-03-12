8a87674fffa8d70ac4f68fe0c83d969bd2c0d334
andrewgwallace <andrew.g.wallace@gmail.com>
2018-03-11 20:01:53 -0400

    Get basic number entry to field in game.html

--- ibetubet/js/script.js
+++ ibetubet/js/script.js
@@ -1,17 +1,21 @@
 // DOM Manipuation
-// Require game.js
 
 document.addEventListener('DOMContentLoaded', function() {
+
+let imported = document.createElement('script');
+imported.src = 'js/game.js';
 
 // Put index.html JS below this line.
 let onePlayer = document.querySelector('input[name="p1Name"]');
 let twoPlayer = document.querySelector('input[name="p2Name"]');
 let startButton = document.querySelector('#start');
-let isReady = false;
+let continueButton = document.querySelector('#continue');
 startButton.disabled = true;
+continueButton.disabled = true;
 
 onePlayer.addEventListener('change', checkReady);
 twoPlayer.addEventListener('change', checkReady);
+// startButton.addEventListener('click')
 
 function checkReady() {
   if (onePlayer.value === '' && twoPlayer.value !== '') {
@@ -26,10 +30,30 @@ function checkReady() {
     startButton.disabled = false;
     startButton.innerHTML = "Let's Play!";
   } else {
-      startButton.innerHTML = "Enter player name(s)";
+    startButton.disabled = true;
+    startButton.innerHTML = "Enter player name(s)";
   }
 }
 
 // Put game.html JS below this line.
 let scoreboard = document.querySelector('.scoreboard');
+
+let numButtons = document.querySelector('.numbers');
+let p1Bet = document.querySelector('input[name="p1bet"]');
+let p1guess = document.querySelector('input[name="p1guess"]');
+let p1Multiplier = document.querySelector('input[name="p1multipler"]');
+let p2Bet = document.querySelector('input[name="p2bet"]');
+let p2guess = document.querySelector('input[name="p2guess"]');
+let p2Multiplier = document.querySelector('input[name="p2multipler"]');
+
+
+function onClick (event) {
+  let number = event.target.innerHTML;
+  p1Bet.value = number;
+  console.log(`You clicked ${event.target}`);
+}
+
+numButtons.addEventListener('click', onClick);
+// window.addEventListener('click', onClick);
+document.head.appendChild(imported);
 });
