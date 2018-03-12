a8819191dd4c7fd61631212ae071852d91df0466
andrewgwallace <andrew.g.wallace@gmail.com>
2018-03-11 20:27:33 -0400

    Disable all other input other than numbers and backspace key

--- ibetubet/js/script.js
+++ ibetubet/js/script.js
@@ -2,58 +2,70 @@
 
 document.addEventListener('DOMContentLoaded', function() {
 
-let imported = document.createElement('script');
-imported.src = 'js/game.js';
+  let imported = document.createElement('script');
+  imported.src = 'js/game.js';
 
-// Put index.html JS below this line.
-let onePlayer = document.querySelector('input[name="p1Name"]');
-let twoPlayer = document.querySelector('input[name="p2Name"]');
-let startButton = document.querySelector('#start');
-let continueButton = document.querySelector('#continue');
-startButton.disabled = true;
-continueButton.disabled = true;
+  // Put index.html JS below this line.
+  let onePlayer = document.querySelector('input[name="p1Name"]');
+  let twoPlayer = document.querySelector('input[name="p2Name"]');
+  let startButton = document.querySelector('#start');
+  let continueButton = document.querySelector('#continue');
+  startButton.disabled = true;
+  continueButton.disabled = true;
 
-onePlayer.addEventListener('change', checkReady);
-twoPlayer.addEventListener('change', checkReady);
-// startButton.addEventListener('click')
+  onePlayer.addEventListener('change', checkReady);
+  twoPlayer.addEventListener('change', checkReady);
+  // startButton.addEventListener('click')
 
-function checkReady() {
-  if (onePlayer.value === '' && twoPlayer.value !== '') {
-    onePlayer.value = twoPlayer.value;
-    twoPlayer.value = '';
-    startButton.disabled = false;
-    startButton.innerHTML = "Let's Play!";
-  } else if (onePlayer.value !== '' && twoPlayer.value !== '') {
-    startButton.disabled = false;
-    startButton.innerHTML = "2 Player Start!";
-  } else if (onePlayer.value !== '' && twoPlayer.value === '') {
-    startButton.disabled = false;
-    startButton.innerHTML = "Let's Play!";
-  } else {
-    startButton.disabled = true;
-    startButton.innerHTML = "Enter player name(s)";
+  function checkReady() {
+    if (onePlayer.value === '' && twoPlayer.value !== '') {
+      onePlayer.value = twoPlayer.value;
+      twoPlayer.value = '';
+      startButton.disabled = false;
+      startButton.innerHTML = "Let's Play!";
+    } else if (onePlayer.value !== '' && twoPlayer.value !== '') {
+      startButton.disabled = false;
+      startButton.innerHTML = "2 Player Start!";
+    } else if (onePlayer.value !== '' && twoPlayer.value === '') {
+      startButton.disabled = false;
+      startButton.innerHTML = "Let's Play!";
+    } else {
+      startButton.disabled = true;
+      startButton.innerHTML = "Enter player name(s)";
+    }
   }
-}
 
-// Put game.html JS below this line.
-let scoreboard = document.querySelector('.scoreboard');
+  // Put game.html JS below this line.
+  let scoreboard = document.querySelector('.scoreboard');
 
-let numButtons = document.querySelector('.numbers');
-let p1Bet = document.querySelector('input[name="p1bet"]');
-let p1guess = document.querySelector('input[name="p1guess"]');
-let p1Multiplier = document.querySelector('input[name="p1multipler"]');
-let p2Bet = document.querySelector('input[name="p2bet"]');
-let p2guess = document.querySelector('input[name="p2guess"]');
-let p2Multiplier = document.querySelector('input[name="p2multipler"]');
+  let numButtons = document.querySelector('.numbers');
+  let p1Bet = document.querySelector('input[name="p1bet"]');
+  let p1guess = document.querySelector('input[name="p1guess"]');
+  let p1Multiplier = document.querySelector('input[name="p1multipler"]');
+  let p2Bet = document.querySelector('input[name="p2bet"]');
+  let p2guess = document.querySelector('input[name="p2guess"]');
+  let p2Multiplier = document.querySelector('input[name="p2multipler"]');
 
 
-function onClick (event) {
-  let number = event.target.innerHTML;
-  p1Bet.value = number;
-  console.log(`You clicked ${event.target}`);
-}
+  function onClick(event) {
+    let number = event.target.innerHTML;
+    p1Bet.value = number;
+    console.log(`You clicked ${event.target}`);
+  }
 
-numButtons.addEventListener('click', onClick);
-// window.addEventListener('click', onClick);
-document.head.appendChild(imported);
+  numButtons.addEventListener('click', onClick);
+  // window.addEventListener('click', onClick);
+  document.head.appendChild(imported);
+
+  //Disable other keyboard input except numbers and backspace
+  document.onkeydown = function(event) {
+    // console.log(event.which);
+    if (event.which < 49 || event.which > 57) {
+      if (event.which === 8) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
 });
