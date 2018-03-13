f0cd9dde36b7862347b3d27000a5eaae9c8e1133
andrewgwallace <andrew.g.wallace@gmail.com>
2018-03-13 10:32:32 -0400

    Rearrange declarations and get ready button functionality working

--- ibetubet/js/script.js
+++ ibetubet/js/script.js
@@ -1,7 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
-// GAME FUNCTIONALITY
-
-// Gameplay functions and variables
+// DECLARATIONS
 
 let p1 = '';
 let p2 = '';
@@ -9,6 +6,8 @@ let p1Bet;
 let p2Bet;
 let p1Guess;
 let p2Guess;
+let p1Multiplier;
+let p2Multiplier;
 let p1Diff = Math.abs(p1Guess - p2Bet);
 let p2Diff = Math.abs(p2Guess - p1Bet);
 let superTieCount = 0;
@@ -21,12 +20,65 @@ let p1RoundsWon = 0;
 let p2RoundsWon = 0;
 let gameWinner = '';
 
-function newGame() {
+// DOM MANIPULATION
 
+let jackpotDisplay = document.querySelector('.jackpot');
+jackpotDisplay.innerHTML = `Jackpot: ${jackpot}`;
+let scoreboard = document.querySelector('.scoreboard');
+const numButtons = document.querySelector('.numbers');
+let p1Score = document.querySelector('.p1points');
+let p2Score = document.querySelector('.p2points');
+p1Score.innerHTML = `Points: ${p1Points}`;
+p2Score.innerHTML = `Points: ${p2Points}`;
+const p1BetBox = document.querySelector('input[name="p1bet"]');
+const p1GuessBox = document.querySelector('input[name="p1guess"]');
+const p1MultiplierBox = document.querySelector('.p1multiplier');
+let p1Ready = document.querySelector('[name="p1ready"]');
+const p2BetBox = document.querySelector('input[name="p2bet"]');
+const p2GuessBox = document.querySelector('[name="p2ready"]')
+const p2MultiplierBox = document.querySelector('.p2multiplier')
+let p2Ready = document.querySelector('[name="p2ready"]');
+let numbox;
+
+document.addEventListener('DOMContentLoaded', function() {
+// GAME FUNCTIONALITY
+function targetClick(event) {
+  if (event.target.className.includes('numbox')) {
+    numbox = event.target;
+  }
+  if (/[1-9]/g.test(event.target.innerText)) {
+    number = event.target.innerText;
+    numbox.value = number;
+  }
+  if (event.target.name === 'p1ready') {
+    // console.log("AWESOME!");
+    event.preventDefault();
+    p1Bet = p1BetBox.value;
+    p1Guess = p1GuessBox.value;
+    p1Multiplier = p1MultiplierBox.value;
+    p1BetBox.value = '';
+    p1GuessBox.value = '';
+    p1Ready.disabled = true;
+  }
+  if (event.target.name === 'p2ready') {
+    // console.log("AWESOME!");
+    event.preventDefault();
+    p2Bet = p1BetBox.value;
+    p2Guess = p1GuessBox.value;
+    p2Multiplier = p1MultiplierBox.value;
+    p2BetBox.value = '';
+    p2GuessBox.value = '';
+    p2Ready.disabled = true;
+  }
 }
 
+document.addEventListener('click', targetClick);
+// function newGame() {
+//
+// }
+
 function playRound () {
-  getBetsandGuesses();
+
   findRoundWinner();
 }
 
@@ -77,51 +129,15 @@ function isGameOver () {
   }
 }
 
-
-// HTML JS
-
-  let scoreboard = document.querySelector('.scoreboard');
-  let numButtons = document.querySelector('.numbers');
-  let p1Score = document.querySelector('.p1points');
-  let p2Score = document.querySelector('.p2points');
-  p1Score.innerHTML = `Points: ${p1Points}`;
-  p2Score.innerHTML = `Points: ${p2Points}`;
-  let p1GuessBox = document.querySelector('input[name="p1guess"]');
-  let p2BetBox = document.querySelector('input[name="p2bet"]');
-  let p2GuessBox = document.querySelector('input[name="p2guess"]');
-  let p1Ready = document.querySelector('input[name="p1ready"]');
-  let p2Ready = document.querySelector('input[name="p2ready"]');
-  let numbox;
+  // function onGoClick (event) {
+  //   //check that both ready buttons are disabled
+  //   // if yes, run roundWinner function (which should include timeout)
+  // }
 
 
-  function targetClick(event) {
-    if (event.target.className.includes('numbox')) {
-      numbox = event.target;
-    }
-    if (/[1-9]/g.test(event.target.innerText)) {
-      number = event.target.innerText;
-      numbox.value = number;
-    }
-    return;
-  }
 
-  function onReadyClick (event) {
-    // checkIfValues
-    // submit numbers and disable
-  }
-
-  function onGoClick (event) {
-    //check that both ready buttons are disabled
-    // if yes, run roundWinner function (which should include timeout)
-  }
-
-  // numButtons.addEventListener('click', targetClick);
-  document.addEventListener('click', targetClick);
-
-  // p1Ready.addEventListener('click', onClick);
-
-  //Disable other keyboard input except numbers and backspace
-  function disableKeys() {
+//Disable other keyboard input except numbers and backspace
+function disableKeys() {
   document.onkeydown = function(event) {
     if (event.which < 49 || event.which > 57) {
       if (event.which === 8) {
@@ -131,11 +147,6 @@ function isGameOver () {
       }
     }
   };
-}
-
-function score () {
-  p1Score.innerHTML = `Points: ${p1Points}`;
-  p2Score.innerHTML = `Points: ${p2Points}`;
 }
 
 disableKeys();
