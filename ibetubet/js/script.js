f5624f307c613cd9d9ad245b1b6f43eb81805651
andrewgwallace <andrew.g.wallace@gmail.com>
2018-03-14 13:26:16 -0400

    Fiddle with scoreboard

--- ibetubet/js/script.js
+++ ibetubet/js/script.js
@@ -165,30 +165,30 @@ function isGameOver () {
 
 let p1Row = document.querySelector('.p1row');
 let p2Row = document.querySelector('.p2row');
-let p1Box = document.createElement('div');
-    p1Box.className = 'scorebox';
-let p2Box = document.createElement('div');
-    p1Box.className = 'scorebox';
+let p1ScoreBox = document.createElement('div');
+let p2ScoreBox = document.createElement('div');
+let scoreBox = document.createElement('div');
+p1ScoreBox.innerHTML = '';
+p2ScoreBox.innerHTML = '';
 // let roundRow = document.querySelector('.roundrow');
 // let scoreBox = document.createElement('div');
 
-  function scoreBoard () {
+  function scoreBoard (e) {
       if (roundWinner === p1) {
-        p1Row.appendChild(p1Box);
-        p1Box.style.backgroundColor = 'green';
-        p2Row.appendChild(p2Box);
-        p2Box.style.backgroundColor = 'red';
-        // p2Row.appendChild(scoreBox).style.backgroundColor = 'red';
+        p1Row.appendChild(p1ScoreBox);
+        p1ScoreBox.innerHTML = '';
+        this.className = 'win';
+        p2Row.appendChild(scoreBox).style.backgroundColor = 'red';
       } else if (roundWinner === p2) {
-        p1Row.appendChild(p1Box);
-        p1Box.style.backgroundColor = 'red';
-        p2Row.appendChild(p2Box);
-        p2Box.style.backgroundColor = 'green';
+        p1Row.appendChild(p1ScoreBox);
+        p1ScoreBox.className += ' lose';
+        p2Row.appendChild(p2ScoreBox);
+        p2ScoreBox.className += ' win';
       } else {
-        p1Row.appendChild(p1Box);
-        p1Box.style.backgroundColor = 'yellow';
-        p2Row.appendChild(p2Box);
-        p2Box.style.backgroundColor = 'yellow';
+        p1Row.appendChild(p1ScoreBox);
+        p1ScoreBox.className += ' tie';
+        p2Row.appendChild(p2ScoreBox);
+        p2ScoreBox.className += ' tie';
       }
       // roundRow.appendChild(scoreBox);
     }
